ed73b6369a1b240ca10e1026cb49c07f54ae95c5
Scott Came <scott@search.org>
2016-10-27 23:44:19 -0700

    Minor cosmetic cleanups; remove unneeded toy data sources, schemas, and users

--- analytics-saiku/files/server-config.sh
+++ analytics-saiku/files/server-config.sh
@@ -2,17 +2,21 @@
 
 #set -x
 
+apk --update add jq
+
+sed -i -r -e 's/securerandom.source.+/securerandom.source=file:\/dev\/urandom/' /opt/jdk/jdk1.8.0_66/jre/lib/security/java.security
+
 cd /opt/saiku-server
 cp start-saiku.sh temp-start-saiku.sh
 sed -i "s/\/opt\/saiku-server\/tomcat\/bin\/catalina.sh run/sh startup.sh/g" /opt/saiku-server/temp-start-saiku.sh
 ./temp-start-saiku.sh
 
 
-CURLTEST='curl -s -u admin:admin http://localhost/saiku/rest/saiku/api/license'
+CURLTEST='curl -s -u admin:admin -m 2 http://localhost/saiku/rest/saiku/api/license'
 
 eval $CURLTEST >> /dev/null 2>&1
 while [ $? -ne 0 ]; do
-       	sleep 5
+       	sleep 2
        	echo "Testing availability of Saiku server..."
    eval $CURLTEST >> /dev/null 2>&1
 done
@@ -22,7 +26,21 @@ echo "Tomcat successfully started"
 curl -sSL https://raw.githubusercontent.com/ojbc/analytics/master/incident-arrest/OJBMondrianSchema.xml -o /tmp/OJBMondrianSchema.xml
 sed -i "s/OJBC Analytics/Incident-Arrest/g" /tmp/OJBMondrianSchema.xml
 
-curl -X POST --data 'language=en&username=admin&password=admin' http://localhost/saiku/rest/saiku/session
+curl -sSL https://raw.githubusercontent.com/ojbc/analytics/master/booking-jail/JailBookingMondrianSchema.xml -o /tmp/JailBookingMondrianSchema.xml
+
+curl -sSl -X POST --data 'language=en&username=admin&password=admin' http://localhost/saiku/rest/saiku/session
+
+echo "Removing default/toy data sources and schemas"
+
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/datasources | jq -r '.[] | select(.connectionname=="earthquakes")' | grep id | sed -r -e 's/.*: "(.*)",?/curl -sSl -u admin:admin -X DELETE http:\/\/localhost\/saiku\/rest\/saiku\/admin\/datasources\/\1/g' | xargs xargs > /dev/null 2>&1
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/datasources | jq -r '.[] | select(.connectionname=="foodmart")' | grep id | sed -r -e 's/.*: "(.*)",?/curl -sSl -u admin:admin -X DELETE http:\/\/localhost\/saiku\/rest\/saiku\/admin\/datasources\/\1/g' | xargs xargs > /dev/null 2>&1
+
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/schema | jq -r '.[] | select(.name=="foodmart4.xml")' | grep name | sed -r -e 's/.*: "(.*)",?/curl -sSl -u admin:admin -X DELETE http:\/\/localhost\/saiku\/rest\/saiku\/admin\/schema\/\1/g' | xargs xargs > /dev/null 2>&1
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/schema | jq -r '.[] | select(.name=="earthquakes.xml")' | grep name | sed -r -e 's/.*: "(.*)",?/curl -sSl -u admin:admin -X DELETE http:\/\/localhost\/saiku\/rest\/saiku\/admin\/schema\/\1/g' | xargs xargs > /dev/null 2>&1
+
+echo "Removing default user smith"
+
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/users/ | jq -r '.[] | select(.username=="smith")' | grep id | sed -r -e 's/.*: (.*)/curl -sSl -u admin:admin -X DELETE http:\/\/localhost\/saiku\/rest\/saiku\/admin\/users\/\1/g' | xargs xargs > /dev/null 2>&1
 
 echo "Installing Mondrian schemas..."
 
@@ -31,8 +49,22 @@ curl -s -u admin:admin -F name="Jail-Booking" -F file="@/tmp/JailBookingMondrian
 
 echo "Installing data sources..."
 
-curl -X POST -u admin:admin --header "Content-Type: application/json" -T /tmp/Incident-Arrest.connection.json http://localhost/saiku/rest/saiku/admin/datasources
-curl -X POST -u admin:admin --header "Content-Type: application/json" -T /tmp/Jail-Booking.connection.json http://localhost/saiku/rest/saiku/admin/datasources
+curl -s -X POST -u admin:admin --header "Content-Type: application/json" -T /tmp/Incident-Arrest.connection.json http://localhost/saiku/rest/saiku/admin/datasources
+curl -s -X POST -u admin:admin --header "Content-Type: application/json" -T /tmp/Jail-Booking.connection.json http://localhost/saiku/rest/saiku/admin/datasources
+
+echo "\nFinal setup:"
+
+echo "\nUsers:"
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/users/
+
+echo "\nSchemas:"
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/schema
+
+echo "\nData sources:"
+curl -sSl -u admin:admin http://localhost/saiku/rest/saiku/admin/datasources
+
+apk del jq
 
 /opt/saiku-server/stop-saiku.sh
+
 rm temp-start-saiku.sh
