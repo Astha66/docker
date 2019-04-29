cbcef6cde556ea79ceb54aa774ce62f4df8bed41
James Douglas <jdouglas@search.org>
2019-04-29 10:24:32 -0400

    Updating rapback docker files

--- rapback-karaf/files/karaf-feature-bundle-install-script-ojb.sh
+++ rapback-karaf/files/karaf-feature-bundle-install-script-ojb.sh
@@ -1,3 +1,50 @@
+bundle:install -s mvn:org.ojbc.bundles.utilities/h2-mock-database
+bundle:install -s mvn:org.ojbc.bundles.shared/ojb-resources-common
 bundle:install -s mvn:org.ojbc.bundles.shared/ojb-osgi-common
-bundle:install -s mvn:org.ojbc.bundles.shared/ojb-camel-common  
+
+bundle:install -s file:///tmp/ojb-common-3.0.2.jar
+
+bundle:install -s mvn:org.ojbc.bundles.shared/ojb-camel-common
 bundle:install -s mvn:org.ojbc.bundles.shared/ojb-fedquery-common
+
+# Entity_Resolution_Resources is being copied to deploy
+#bundle:install mvn:org.ojbc.bundles.shared/Entity_Resolution_Resources
+
+bundle:install -s mvn:org.ojbc.bundles.intermediaries/Entity_Resolution_Service_Intermediary_OSGi
+bundle:install -s mvn:org.ojbc.bundles.intermediaries/Entity_Resolution_Service_Intermediary
+
+
+bundle:install -s file:///tmp/ojb-enhanced-audit-common-1.0.3.jar
+bundle:install -s file:///tmp/person-search-request-service-intermediary-1.2.0.jar
+bundle:install -s file:///tmp/subscription-notification-service-intermediary-common-2.0.11.jar
+bundle:install -s file:///tmp/audit-rest-utility-1.0.4.jar
+bundle:install -s file:///tmp/subscription-notification-service-intermediary-1.0.8.jar
+bundle:install -s file:///tmp/person-query-service-criminal-history-intermediary-1.0.6.jar
+bundle:install -s file:///tmp/policy-acknowledgement-service-intermediary-1.0.2.jar
+bundle:install -s file:///tmp/ojb-policy-decision-point-service-1.0.2.jar
+bundle:install -s file:///tmp/arrest-reporting-service-intermediary-1.0.6.jar
+bundle:install -s file:///tmp/identification-reporting-service-intermediary-1.0.2.jar
+bundle:install -s file:///tmp/organization-identification-results-search-service-intermediary-1.0.2.jar
+bundle:install -s file:///tmp/organization-identification-results-query-service-intermediary-1.0.2.jar
+bundle:install -s file:///tmp/identification-results-modification-service-intermediary-1.0.1.jar
+bundle:install -s file:///tmp/criminal-identification-reporting-service-intermediary-1.0.0.jar
+#bundle:install -s file:///tmp/parole-event-handler-service-intermediary-1.0.1.jar
+bundle:install -s file:///tmp/disposition-reporting-service-intermediary-1.0.3.jar
+bundle:install -s file:///tmp/firearm-search-request-service-intermediary-1.0.1.jar
+bundle:install -s file:///tmp/firearm-registration-query-request-service-intermediary-1.0.2.jar
+bundle:install -s file:///tmp/person-query-service-warrants-intermediary-1.0.2.jar
+#bundle:install -s file:///tmp/probation-event-handler-service-intermediary-1.0.3.jar
+
+bundle:install -s file:///tmp/arrest-reporting-service-intermediary-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/firearm-registration-query-request-service-intermediary-hawaii-1.0.1.jar
+bundle:install -s file:///tmp/firearm-search-request-service-intermediary-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/parole-event-handler-service-intermediary-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/person-query-service-criminal-history-intermediary-hawaii-1.0.1.jar
+bundle:install -s file:///tmp/person-query-service-warrants-intermediary-hawaii-1.0.1.jar
+bundle:install -s file:///tmp/person-search-request-service-intermediary-hawaii-1.0.1.jar
+bundle:install -s file:///tmp/policy-acknowledgement-service-intermediary-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/probation-event-handler-service-intermediary-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/subscription-notification-service-intermediary-hawaii-1.0.2.jar
+bundle:install -s file:///tmp/ojb-policy-decision-point-service-hawaii-1.0.0.jar
+bundle:install -s file:///tmp/hawaii-resources-1.0.0.jar
+bundle:install -s file:///tmp/disposition-reporting-service-intermediary-hawaii-1.0.0.jar
--- rapback-karaf/files/karaf-feature-bundle-install-script-spring-jdbc.sh
+++ rapback-karaf/files/karaf-feature-bundle-install-script-spring-jdbc.sh
@@ -4,16 +4,22 @@ feature:install cxf-ws-security/3.0.6
 feature:install camel-http4/2.14.4
 feature:install camel-test/2.14.4
 feature:install camel-mail/2.14.4
-feature:install camel-quartz/2.14.4
-feature:install pax-jetty
+feature:install camel-quartz2
+
+# Not used in Rapback
+#feature:install pax-jetty
+
 feature:install spring-security
+
 bundle:install -s mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.commons-lang
 bundle:install -s mvn:org.apache.httpcomponents/httpcore-osgi/4.2.5
 bundle:install -s mvn:org.apache.httpcomponents/httpclient-osgi/4.2.5
 bundle:install -s mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.commons-io
+
 bundle:install -s mvn:com.fasterxml.jackson.core/jackson-core/2.1.5
 bundle:install -s mvn:com.fasterxml.jackson.core/jackson-annotations/2.1.5
 bundle:install -s mvn:com.fasterxml.jackson.core/jackson-databind/2.1.5
+
 bundle:install -s mvn:commons-pool/commons-pool/1.6
 bundle:install -s mvn:commons-codec/commons-codec/1.6
 bundle:install -s mvn:commons-collections/commons-collections/3.2.2
@@ -23,10 +29,13 @@ bundle:install -s mvn:org.osgi/org.osgi.enterprise/4.2.0
 bundle:install -s mvn:com.h2database/h2/1.3.174
 bundle:install -s mvn:com.mysql.jdbc/com.springsource.com.mysql.jdbc
 bundle:install -s mvn:org.apache.commons/commons-math3/3.2
+
 bundle:install -s mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.dom4j
 bundle:install -s mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.guava/11_1
 bundle:install -s mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jaxen/1.1.4_2
 bundle:install -s mvn:joda-time/joda-time/1.6.2
+bundle:install -s mvn:org.wso2.balana/org.wso2.balana
+
 bundle:install -s mvn:com.mysql.jdbc/com.springsource.com.mysql.jdbc  
 bundle:install -s mvn:org.apache.commons/commons-math3/3.2  
 bundle:install -s mvn:org.codehaus.jackson/jackson-core-asl/1.9.5
