c738d86b9c53f2c7b805c41d6e5751a9176a08f2
James Douglas <jdouglas@search.org>
2019-08-07 11:44:20 -0400

    Updating final version of cerbot for Letsencrypt creation and renewal.

---
+++ certbot/letsencrypt-renewal-dry-run.sh
@@ -0,0 +1,8 @@
+docker run -it --rm \
+--mount type=bind,source=/apache-certs,target=/apache-certs \
+--mount type=bind,source=/apache-webroot,target=/apache-webroot \
+--mount type=bind,source=/opt/data/search/docker-mounts/letsencrypt-etc,target=/etc/letsencrypt \
+--name cerbot ojbc/certbot \
+certbot renew --dry-run --webroot -w /apache-webroot -d demo.search.org -n
+sudo cp /opt/data/search/docker-mounts/letsencrypt-etc/live/demo.search.org/*.pem /opt/data/search/docker-mounts/demo.search.org
+docker restart -t 5 demo-search-proxy
